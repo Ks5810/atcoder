f7a5095c0d1a3deae8a865e4164bec220e86b02b
Ks5810 <e40keisuke@gmail.com>
2020-07-18 21:17:00 -0400

    abc084

---
+++ abc084/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ll x, y;
+    int res = 0;
+    cin >> x >> y;
+
+    ll i = x;
+    while(i <= y)
+    {
+        i = i * 2;
+        res++;
+    }
+
+    cout << res << "\n";
+    return 0;
+}
