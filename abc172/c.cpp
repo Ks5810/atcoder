0b4805d86a3a26d9dd6c1f4ba427672cf34f8f44
Ks5810 <e40keisuke@gmail.com>
2020-07-20 13:16:25 -0400

    corrected c.cpp

--- abc172/c.cpp
+++ abc172/c.cpp
@@ -8,31 +8,29 @@ int main()
     int res = 0;
     cin >> n >> m >> k;
 
-    vector<ll> a(n, 0), b(m, 0);
+    vector<ll> a(n + 1, 0), b(m + 1, 0);
     
-    cin >> a[0];
-    for(int i = 1; i < n; ++i)
+    for(int i = 1; i <= n; ++i)
     {
         cin >> a[i];
-        a[i] = a[i] + a[i - 1];
+        a[i] += a[i - 1];
     }
 
-    cin >> b[0];
-    for(int i = 1; i < m; ++i)
+    for(int i = 1; i <= m; ++i)
     {
         cin >> b[i];
-        b[i] = b[i] + b[i - 1];
+        b[i] += b[i - 1];
     }
 
-    int j = m - 1;
-    for(int i = 0; i < n; ++i)
+    int j = m;
+    for(int i = 0; i <= n; ++i)
     {
         if(a[i] > k) break; 
-        while(b[j] > k - a[i])
+        while(a[i] + b[j] > k)
         {
             j--;
         }
-        res = max(res, i + j + 2);
+        res = max(res, i + j);
     }
 
     cout << res << "\n";
