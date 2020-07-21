acb4df7a1f57adf1cf79d4a74a3404a3019493f4
Ks5810 <e40keisuke@gmail.com>
2020-07-20 23:12:27 -0400

    abc169

--- abc169/c.cpp
+++ abc169/c.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using ld = long double;
 
 int main()
 {
-    ll a; ld b;
+    ll a;
+    double b;
     cin >> a >> b;
-    
-    cout << ll(a * (b * 100) / 100) << "\n";
+
+    ll rb = round(b * 100);
+    cout << (a * rb) / 100 << "\n";
     return 0;
 }
