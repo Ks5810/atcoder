5148b66aa81c5043f5eb7f1ecdb736259ddd6a98
Ks5810 <e40keisuke@gmail.com>
2020-09-18 02:07:48 -0400

    lib.cpp

---
+++ lib.cpp
@@ -0,0 +1,124 @@
+// Library for atcoder
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using dl = long double;
+int mod = 1000000007;
+
+// constants
+
+const int dx[4] = { 1, 0,-1, 0};
+const int dy[4] = { 0, 1, 0,-1};
+
+
+// numbers
+
+bool is_prime(ll n) {
+    if (n <= 1) 
+        return false;
+
+    for (ll i = 2; i * i <= n; ++i) {
+        if (n % i == 0) 
+            return false;
+        }
+    return true;
+}
+
+
+map<ll, ll> prime_factorize(ll n)
+{
+    map<ll, ll> res;
+    
+    for (ll i = 2; i * i <= n; ++i) {
+        while (n % i == 0) 
+        {
+            res[i]++;
+            n /= i;
+        }
+    }
+    if (n != 1) res[n] = 1;
+
+    return res;
+}
+
+
+vector<ll> divisor(ll n) 
+{
+    vector<ll> res;
+
+    for (ll i = 1l; i * i <= n; ++i) 
+    {
+        if (n % i == 0)
+        {
+            res.push_back(i);
+            ll j = n / i;
+            
+            if (j != i) res.push_back(j);
+        }
+    }
+    sort(res.begin(), res.end());
+    return res;
+}
+
+
+ll gcd(ll a, ll b)
+{
+    if(a % b == 0)
+        return b;
+    
+    return gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b)  
+{  
+     return (a * b) / gcd(a, b);  
+}  
+
+
+ll powMod(ll x, ll n)
+{
+    ll res = 1;
+
+    for(ll i = 0; i < n; i++)
+        res = res * x % mod;
+    
+    return res;
+}
+
+
+ll digitSum(ll n)
+{
+    if(n < 10) 
+        return n;
+
+    return digitSum(n / 10) + n % 10;
+}
+
+
+// strings
+
+bool isPalindrome(string s)
+{
+    string t(s.rbegin(), s.rend());
+    return s == t;
+}
+
+
+// utils
+
+string paddingRight(string s, int width, char fill) 
+{
+    ostringstream oss;
+    oss << setw(width) << setfill(fill) << s;
+    return oss.str();
+}
+
+
+
+string paddingLeft(string s, int width, char fill) 
+{
+    ostringstream oss;
+    oss << left << setw(width) << setfill(fill) << s;
+    return oss.str();
+}
